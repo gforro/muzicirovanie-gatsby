1e5f3ef6267cc57eaaba18424dca62fa5ffcc63e
Gabriel Forro <gabriel.forro@gmail.com>
2020-06-15 14:32:13 +0200

    add chakra-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Box, Text } from '@chakra-ui/core';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+function IndexPage() {
+  return (
+    <Box p={8} bg="teal.500">
+      <Text fontSize="6xl" color="white">Muzicirovanie</Text>
+    </Box>
+  );
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default IndexPage;
